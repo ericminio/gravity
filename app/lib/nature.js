d05d3457f964e06d83de4890f5b323d3d19bea9a
ericminio <eric.mignot@gmail.com>
2014-12-24 14:10:07 -0500

    exploring acceleration

--- app/lib/nature.js
+++ app/lib/nature.js
@@ -1,4 +1,6 @@
-module.exports = {
+var nature = {
+
+    g: 10,
 
     o: undefined,
 
@@ -12,9 +14,18 @@ module.exports = {
     },
     
     move: function(dt) {
+        this.acceleration.ax = 0;
+        this.acceleration.ay = 0;
+        this.acceleration.az = -nature.g;
+        
+        this.speed.vx = this.speed.vx + dt * this.acceleration.ax;
+        this.speed.vy = this.speed.vy + dt * this.acceleration.ay;
+        this.speed.vz = this.speed.vz + dt * this.acceleration.az;
+        
         this.position.x = this.position.x + dt * this.speed.vx;
         this.position.y = this.position.y + dt * this.speed.vy;
         this.position.z = this.position.z + dt * this.speed.vz;        
     },
 };
 
+module.exports = nature;
--- app/movement.spec.js
+++ app/movement.spec.js
@@ -2,14 +2,34 @@ var nature = require('./lib/nature');
 
 describe('Movement', function() {
 
+    it('is created by gravity', function() {
+        var ball = {
+            position: { x:0, y: 0, z:100 },
+            speed: { vx:0, vy: 0, vz: 0 },
+            acceleration: { ax:0, ay:0, az:0 }
+        };
+        nature.willImpact(ball);
+        nature.travelInTime(1);
+
+        expect(ball.acceleration).toEqual({ ax:0, ay:0, az:-nature.g });
+        expect(ball.speed).toEqual({ vx:0, vy:0, vz:-nature.g })
+        expect(ball.position).toEqual({ x:0, y:0, z:(100-nature.g) });
+    });
+
     it('comes from speed and time', function() {
         var plane = {
-            position: { x:0, y: 0, z:0 },
-            speed: { vx:1, vy: 2, vz: 3 }
+            position: { x:0, y:0, z:100 },
+            speed: { vx:1, vy:2, vz:3 },
+            acceleration: { ax:0, ay:0, az:0 }
         };
         nature.willImpact(plane);
-        nature.travelInTime(2);
+        nature.travelInTime(1);
         
-        expect(plane.position).toEqual({ x:2, y: 4, z:6 });
-    });
+        expect(plane.speed).toEqual({ vx:1, vy:2, vz:3-nature.g })
+        expect(plane.position).toEqual({ x:1, y:2, z:100+3-nature.g });
+        nature.travelInTime(1);
+        
+        expect(plane.speed).toEqual({ vx:1, vy:2, vz:3-nature.g-nature.g });
+        expect(plane.position).toEqual({ x:2, y:4, z:100+3-nature.g+3-nature.g-nature.g });
+    });    
 });
